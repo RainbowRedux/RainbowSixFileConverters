1dff7d957bf7f6714cce173f876b7f7922cb4358
Philip Edwards <philip@gdcorner.com>
2018-12-01 21:50:21 +1100

    Added test map folder to mapconverter script

--- MAPConverter.py
+++ MAPConverter.py
@@ -23,8 +23,9 @@ def main():
     """Main function that converts a test file"""
     paths = []
     #paths.append("../Data/Test")
-    paths.append("../Data/R6GOG")
-    paths.append("../Data/RSDemo")
+    paths.append("../Data/Test/Maps")
+    #paths.append("../Data/R6GOG")
+    #paths.append("../Data/RSDemo")
 
     fp = DirectoryProcessor.DirectoryProcessor()
     fp.paths = fp.paths + paths
