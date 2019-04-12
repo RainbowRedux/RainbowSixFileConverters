ed511103b6035bd65a3d2f8ac5a5b7a9913bd465
Philip Edwards <philip@gdcorner.com>
2019-04-12 21:57:25 +1000

    Added a mission file reader for R6
    - Added a mission file reader for R6. RS support to come later.
    - Added some utility functions to TextFileUtilities
    - Fixed error with text file parsing and missing quotations.

--- FileUtilities/TextFileUtilities.py
+++ FileUtilities/TextFileUtilities.py
@@ -13,12 +13,20 @@ def read_tokenized_text_file(path):
         filteredLine = line
         if commentStart > -1:
             filteredLine = filteredLine[:commentStart]
-        line_values = shlex.split(filteredLine)
+        line_values = []
+        try:
+            line_values = shlex.split(filteredLine)
+        except ValueError as ve:
+            # Some files are incorrectly written, for instance M09 in Rainbow Six is missing a quotation close for an operator name
+            # Unfortunately since the original files can't be patched this code needs to be forgiving around errors
+            if ve.args == "No closing quotation":
+                line_values = shlex.split(filteredLine + "\"")
         for value in line_values:
             keywords.append(value)
     return keywords
 
 def read_text_dict(path):
+    """Reads a tokenized text file and assumes that it's full of key/value pairs"""
     new_dict = {}
     tokens = read_tokenized_text_file(path)
     while len(tokens) > 1:
@@ -27,3 +35,11 @@ def read_text_dict(path):
         new_dict[new_key] = new_value
 
     return new_dict
+
+def read_keyword_list(keywords, list_length):
+    """Reads a number of tokens froma keyword list and returns as separate list"""
+    newlist = []
+    #Uses a loop like this to ensure the original list is modified when removing keywords, rather than a just this reference to the list updating
+    for _ in range(list_length):
+        newlist.append(keywords.pop(0))
+    return newlist
---
+++ RainbowFileReaders/R6MissionReader.py
@@ -0,0 +1,66 @@
+"""Classes and functions to load and parse Mission files from Rainbow Six"""
+from RainbowFileReaders import R6Settings
+from FileUtilities.TextFileUtilities import read_tokenized_text_file, read_keyword_list
+
+class R6MissionFile(object):
+    """Mission description file for Rainbow Six"""
+    def __init__(self):
+        super(R6MissionFile, self).__init__()
+    
+    def read_room_list(self, keywords):
+        room_list = []        
+        nextKeyword = keywords[0]
+        while nextKeyword != "End":
+            room_list.append(keywords.pop(0))
+            nextKeyword = keywords[0]
+        #Discard the "End" keyword
+        keywords.pop(0)
+        return room_list
+
+    def load_mission(self, path):
+        pass
+
+        self.filepath = path
+        keywords = read_tokenized_text_file(path)
+
+        self.map_file_name = keywords.pop(0)
+        self.map_directory = keywords.pop(0)
+        self.mission_name = keywords.pop(0)
+        self.default_plan_filename = keywords.pop(0)
+        self.briefing_filename = keywords.pop(0)
+        self.debrief_filename = keywords.pop(0)
+        self.mission_picture_filename = keywords.pop(0)
+
+        self.render_hardware_near_clip = float(keywords.pop(0).replace("f",""))
+        self.render_hardware_far_clip = float(keywords.pop(0).replace("f",""))
+        self.render_software_near_clip = float(keywords.pop(0).replace("f",""))
+        self.render_software_far_clip = float(keywords.pop(0).replace("f",""))
+
+        self.render_fog_enabled = keywords.pop(0)
+        self.render_fog_color = read_keyword_list(keywords, 3)
+        self.render_fog_start_distance = float(keywords.pop(0).replace("f",""))
+        self.render_fog_end_distance = float(keywords.pop(0).replace("f",""))
+
+        self.render_ambient_light_color = read_keyword_list(keywords, 3)
+        self.render_background_color = read_keyword_list(keywords, 3)
+
+        self.clouds_enabled = keywords.pop(0)
+        self.clouds_texture_filename = keywords.pop(0)
+        self.clouds_texture_multiplier = float(keywords.pop(0).replace("f",""))
+        self.clouds_speed_multiplier = float(keywords.pop(0).replace("f",""))
+
+        outdoorRoomsKeyword = keywords.pop(0)
+        if outdoorRoomsKeyword != "OutdoorRooms":
+            raise ValueError("OutdoorRooms keyword expected but not found. Found: " + str(outdoorRoomsKeyword))
+            return
+
+        self.outdoor_rooms = self.read_room_list(keywords)
+
+        fogRoomsKeyword = keywords.pop(0)
+        if fogRoomsKeyword != "FogRooms":
+            raise ValueError("FogRooms keyword expected but not found. Found: " + str(fogRoomsKeyword))
+            return
+
+        self.fog_rooms = self.read_room_list(keywords)
+
+        #TODO: Read background colors list and onwards
