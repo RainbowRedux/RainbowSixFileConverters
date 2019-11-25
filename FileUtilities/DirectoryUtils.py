67056950edd8c125ec2d010fa21df3bb7c5eb257
Philip Edwards <philip@gdcorner.com>
2019-11-25 12:39:22 +1100

    Update DirectoryUtils.py

--- FileUtilities/DirectoryUtils.py
+++ FileUtilities/DirectoryUtils.py
@@ -2,7 +2,7 @@
 Commonly used functions for directories
 """
 import os
-from os.path import join
+from os import path
 
 def gather_files_in_path(extension, folder):
     """Walks a folder and it's sub directories and finds all files with matching extension, case-insensitive"""
@@ -10,8 +10,7 @@ def gather_files_in_path(extension, folder):
     for root, dirs, files in os.walk(folder, topdown=True):
         for name in files:
             if name.upper().endswith(extension.upper()):
-                filesToProcess.append(join(root, name))
+                filesToProcess.append(path.join(root, name))
         for name in dirs:
             pass
-            #print("Walking directory: " + os.path.join(root, name))
     return filesToProcess
