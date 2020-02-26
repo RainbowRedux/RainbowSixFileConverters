0c070b84078efeed3379c364947bafd85f793f9e
Philip Edwards <philip@gdcorner.com>
2020-02-26 18:20:00 +1100

    Removed old settings.

--- UnrealImporters/ImporterSettings.py
+++ UnrealImporters/ImporterSettings.py
@@ -1,11 +1,2 @@
-#map_file_path = """D:\R6Data\TestData\ReducedGames\R6GOG\data\map\m09\M09.map"""
-map_file_path = """D:/R6Data/TestData/ReducedGames/R6GOG/data/map/m01/M01.map"""
-#map_file_path = """D:/R6Data/TestData/ReducedGames/RSDemo/mods/CLASSIC MISSIONS/map/cl02/cl02.map"""
-#map_file_path = """D:/R6Data/TestData/ReducedGames/RSDemo/data/map/rm01/rm01.map"""
-#map_file_path = """D:/R6Data/TestData/ReducedGames/R6GOG/data/map/m04/M04.map"""
-#map_file_path = """D:/R6Data/TestData/ReducedGames/R6GOG/data/map/m07/M7.map"""
-#map_file_path = """D:/R6Data/TestData/ReducedGames/R6GOG/data/map/m02/mansion.map"""
-#map_file_path = """D:/R6Data/TestData/ReducedGames/R6GOG/data/map/m09/M09.map"""
-
 PNG_CACHE_FILE_SUFFIX = ".CACHE.PNG"
 bUsePNGCache = True
