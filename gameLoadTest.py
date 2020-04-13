e54b43d14c1414487b7c3badc1c67ee803e7b57f
Philip Edwards <philip@gdcorner.com>
2020-04-13 18:43:23 +1000

    Cleaned up logging in gameLoadTest.py

--- gameLoadTest.py
+++ gameLoadTest.py
@@ -1,8 +1,14 @@
 """A quick sample program to try loading games and listing missions"""
 
+import logging
+
 from RainbowFileReaders.RSEGameLoader import RSEGameLoader
 from RainbowFileReaders.R6MissionReader import R6MissionFile
 from FileUtilities.Settings import load_settings
+
+log = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
     settings = load_settings()
@@ -14,9 +20,10 @@ if __name__ == "__main__":
 
     for _, missionPath in missions.items():
         mission = R6MissionFile()
-        print("Loading: " + missionPath)
+        log.info("Loading: %s", missionPath)
         mission.load_mission(missionPath)
 
-        print(mission.render_hardware_far_clip)
+        log.info("Hardware far clip: %f", mission.render_hardware_far_clip)
+        log.info("List of rooms with fog:")
         for fogroom in mission.fog_rooms:
-            print(fogroom)
+            log.info("\t- %s", fogroom)
