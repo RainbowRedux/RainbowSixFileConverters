7a5ad86e0ab1552f82a76bda89cc18215c344040
Philip Edwards <philip@gdcorner.com>
2020-04-13 18:35:38 +1000

    Added docstrings to settings.py

--- FileUtilities/Settings.py
+++ FileUtilities/Settings.py
@@ -1,6 +1,8 @@
+"""Provides a simple interface to load a settings file"""
 import json
 
 def load_settings(filename="settings.json"):
+    """Loads a json file that contains the settings desired"""
     settingsFile = open(filename)
     settings = json.load(settingsFile)
     settingsFile.close()
