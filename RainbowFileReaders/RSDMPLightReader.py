6d91d944c5738123760c94166333613085031d60
Philip Edwards <philip@gdcorner.com>
2020-04-16 17:32:52 +1000

    Added annotations to RSDMPLightReader

--- RainbowFileReaders/RSDMPLightReader.py
+++ RainbowFileReaders/RSDMPLightReader.py
@@ -1,12 +1,16 @@
 """ This module contains classes to read DMP files that are associated with Rainbow Six Rogue Spear maps. These are light definitions """
-from FileUtilities.BinaryConversionUtilities import BinaryFileDataStructure, FileFormatReader, SizedCString
+
+from typing import List
+
+from FileUtilities.BinaryConversionUtilities import BinaryFileDataStructure, FileFormatReader, SizedCString, BinaryFileReader
+
 
 class RSDMPLightFile(FileFormatReader):
     """Class to read full RSB files"""
     def __init__(self):
         super(RSDMPLightFile, self).__init__()
-        self.header = None
-        self.lights = []
+        self.header: RSDMPHeader = RSDMPHeader()
+        self.lights: List[RSDMPLight] = []
 
     def read_data(self):
         super().read_data()
@@ -27,45 +31,45 @@ class RSDMPHeader(BinaryFileDataStructure):
     def __init__(self):
         super(RSDMPHeader, self).__init__()
 
-    def read(self, filereader):
+    def read(self, filereader: BinaryFileReader):
         super().read(filereader)
 
-        self.FileID = filereader.read_uint32()
-        self.AmbientLightColor = filereader.read_vec_f(4)
-        self.unknown5 = filereader.read_uint32()
-        self.lightCount = filereader.read_uint32()
+        self.FileID: int = filereader.read_uint32()
+        self.AmbientLightColor: List[float] = filereader.read_vec_f(4)
+        self.unknown5: int = filereader.read_uint32()
+        self.lightCount: int = filereader.read_uint32()
 
 class RSDMPLight(BinaryFileDataStructure):
     """Reads and stores Light structures from Rogue spear DMP files."""
     def __init__(self):
         super(RSDMPLight, self).__init__()
 
-    def read(self, filereader):
+    def read(self, filereader: BinaryFileReader):
         super().read(filereader)
 
-        self.parent_room_string = SizedCString(filereader)
+        self.parent_room_string: SizedCString = SizedCString(filereader)
 
-        self.name_string = SizedCString(filereader)
+        self.name_string: SizedCString = SizedCString(filereader)
         if self.name_string.string == "Version":
-            self.version_string = self.name_string
-            self.versionNumber = filereader.read_uint32()
+            self.version_string: SizedCString = self.name_string
+            self.versionNumber: int = filereader.read_uint32()
             self.name_string = SizedCString(filereader)
 
-            self.unknown6 = filereader.read_bytes(1)[0]
+            self.unknown6: int = filereader.read_bytes(1)[0]
 
-        self.lightType = filereader.read_uint32()
-        self.direction = filereader.read_vec_f(3)
-        self.position = filereader.read_vec_f(3)
-        self.falloff = filereader.read_float()
-        self.unknown2 = filereader.read_vec_f(2)
-        self.unknown3 = filereader.read_vec_f(3)
+        self.lightType: int = filereader.read_uint32()
+        self.direction: List[float] = filereader.read_vec_f(3)
+        self.position: List[float] = filereader.read_vec_f(3)
+        self.falloff: float = filereader.read_float()
+        self.unknown2: List[float] = filereader.read_vec_f(2)
+        self.unknown3: List[float] = filereader.read_vec_f(3)
 
-        self.energy = filereader.read_float()
-        self.diffuseColor = filereader.read_vec_f(4)
-        self.specularColor = filereader.read_vec_f(4)
-        self.ambientColor = filereader.read_vec_f(4)
-        self.constantAttenuation = filereader.read_float()
-        self.linearAttenuation = filereader.read_float()
-        self.quadraticAttenuation = filereader.read_float()
-        self.spotlightConeAngle = filereader.read_float()
-        self.type = filereader.read_bytes(1)[0]
+        self.energy: float = filereader.read_float()
+        self.diffuseColor: List[float] = filereader.read_vec_f(4)
+        self.specularColor: List[float] = filereader.read_vec_f(4)
+        self.ambientColor: List[float] = filereader.read_vec_f(4)
+        self.constantAttenuation: float = filereader.read_float()
+        self.linearAttenuation: float = filereader.read_float()
+        self.quadraticAttenuation: float = filereader.read_float()
+        self.spotlightConeAngle: float = filereader.read_float()
+        self.type: int = filereader.read_bytes(1)[0]
