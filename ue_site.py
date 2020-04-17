9bbc7968ef85417617cb5366d47d7de3e429190c
Philip Edwards <philip@gdcorner.com>
2020-04-17 12:16:44 +1000

    Fixed mypy compatiblity with ue_site.py

--- ue_site.py
+++ ue_site.py
@@ -1,9 +1,9 @@
 """This module should import and initialize all modules required by unreal"""
 
-from PIL import Image
+from PIL import Image # type: ignore
 
-import unreal_engine
-from unreal_engine.classes import Blueprint
+import unreal_engine # type: ignore
+from unreal_engine.classes import Blueprint # type: ignore
 
 from UnrealImporters.ImportSOB import RSEResourceLoader
 from UnrealImporters import ImportSOB
